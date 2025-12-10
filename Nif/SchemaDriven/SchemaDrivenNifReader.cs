019c3b8ce2ffb41a477c25c940365d76f0709a3d
Zusk <kingofbubbles@gmail.com>
2025-12-10 01:20:53 -0500

    Handle optional NIF group list

--- Nif/SchemaDriven/SchemaDrivenNifReader.cs
+++ Nif/SchemaDriven/SchemaDrivenNifReader.cs
@@ -27,7 +27,9 @@ public sealed class SchemaDrivenNifReader
 
         // String palette (and optional groups) are present in most NIF versions and we need to advance past them
         NifStringPalette.ReadStrings(br);
-        NifGroupList.Read(br);
+        var groupsInfo = NifGroupList.Read(br);
+        if (!groupsInfo.Consumed)
+            Console.WriteLine("[NIF] GROUPS section not detected; continuing with zero groups.");
 
         var file = new NifFile { Header = header };
 
